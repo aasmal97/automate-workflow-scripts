28acf654b624396e33b612bddcd9c242f2e34082
Arky <arkyasmal@gmail.com>
2021-10-31 15:22:24 -0400

    "check if open already for Divvy"

--- openStockApp.bat
+++ openStockApp.bat
@@ -1,2 +1,2 @@
 start "" "C:\Users\Arky Asmal\AppData\Local\Programs\Microsoft VS Code\Code.exe" "C:\Users\Arky Asmal\OneDrive\Desktop\Personal Projects\Stock Tracker"
-start "" "C:\Users\Arky Asmal\AppData\Local\Mizage LLC\Divvy\Divvy.exe"
+TASKLIST | FINDSTR Divvy || START "" "C:\Users\Arky Asmal\AppData\Local\Mizage LLC\Divvy\Divvy.exe"
--- openVisualum.bat
+++ openVisualum.bat
@@ -1,2 +1,2 @@
 start "" "C:\Users\Arky Asmal\AppData\Local\Programs\Microsoft VS Code\Code.exe" "C:\Users\Arky Asmal\OneDrive\Desktop\Personal Projects\Visualum\visualum"
-start "" "C:\Users\Arky Asmal\AppData\Local\Mizage LLC\Divvy\Divvy.exe"
+TASKLIST | FINDSTR Divvy || START "" "C:\Users\Arky Asmal\AppData\Local\Mizage LLC\Divvy\Divvy.exe"
