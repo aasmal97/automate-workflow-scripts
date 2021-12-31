46d6b78e83e4ac5fd393ca2f81148bdadb2d4560
Arky Asmal <arkyasmal@gmail.com>
2021-12-30 21:43:18 -0500

    "adjusted workflow scripts to accomadate directory changes"

--- openStockApp.bat
+++ openStockApp.bat
@@ -1,2 +1,2 @@
-start "" "C:\Users\Arky Asmal\AppData\Local\Programs\Microsoft VS Code\Code.exe" "C:\Users\Arky Asmal\OneDrive\Desktop\Personal Projects\Stock Tracker"
+start "" "C:\Users\Arky Asmal\AppData\Local\Programs\Microsoft VS Code\Code.exe" "C:\Users\Arky Asmal\Documents\Personal Projects\Stock Tracking App"
 TASKLIST | FINDSTR Divvy || START "" "C:\Users\Arky Asmal\AppData\Local\Mizage LLC\Divvy\Divvy.exe"
